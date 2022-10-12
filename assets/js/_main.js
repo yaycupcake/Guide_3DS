8dfdf5acc2701b82f510044f01c0a2178fe144b3
lifehackerhansol <lifehacker@hansol.ca>
2022-10-12 09:28:58 -0700

    Update progress table
    Fix HBL-USM page
    Fix USM page

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -214,6 +214,7 @@ $(document).ready(function() {
 	  "homebrew-launcher-(pichaxx)": "11",
     "installing-boot9strap-(kartdlphax)": "12",
 	  "bannerbomb3-fredtool-(twn)": "13",
+    "installing-boot9strap-(hbl-usm)": "14"
   };
 
   for(var device in devices){
@@ -254,11 +255,12 @@ $(document).ready(function() {
       "6": ["installing-boot9strap-(hardmod)", "finalizing-setup"],
       "7": ["seedminer", "multiple-options", "finalizing-setup"],
       "8": ["seedminer", "multiple-options", "installing-boot9strap-(fredtool)", "finalizing-setup"],
-      "9": ["seedminer", "bannerbomb3", "installing-boot9strap-(fredtool)", "finalizing-setup"],
-      "10": ["seedminer", "installing-boot9strap-(usm)", "finalizing-setup"],
+      "9": ["seedminer", "bannerbomb3", "multiple-options", "finalizing-setup"],
+      "10": ["seedminer", "bannerbomb3", "installing-boot9strap-(usm)", "finalizing-setup"],
       "11": ["seedminer", "homebrew-launcher-(pichaxx)", "installing-boot9strap-(hbl-usm)", "finalizing-setup"],
       "12": ["installing-boot9strap-(kartdlphax)", "finalizing-setup"],
       "13": ["seedminer", "bannerbomb3-fredtool-(twn)", "finalizing-setup"],
+      "14": ["seedminer", "homebrew-launcher-(pichaxx)", "installing-boot9strap-(hbl-usm)", "finalizing-setup"]
     }
     // Can add custom routing if necessary but currently both routes are identical
     var device_old =  Object.assign({}, device_common,{
